94bb295f0efe4611f166de54ee5c32c92c753b24
Antoine Gelloz <antoine.gelloz@me.com>
2022-10-22 14:48:40 +0200

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {createRoot} from "react-dom/client";
 import {ChakraProvider} from '@chakra-ui/react'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider, useRouteError} from "react-router-dom";
 import Root from './root'
 import AdDetails, {loader as adLoader} from "./adDetails";
 import {extendTheme} from "@chakra-ui/react"
@@ -23,13 +23,22 @@ const router = createBrowserRouter([
     {
         path: "/",
         element: <Root/>,
+        errorElement: <ErrorBoundary/>,
     },
     {
         path: "ads/:adId",
         loader: adLoader,
         element: <AdDetails/>,
+        errorElement: <ErrorBoundary/>,
     },
 ]);
+
+function ErrorBoundary() {
+    let error = useRouteError();
+    console.error(error);
+    // Uncaught ReferenceError: path is not defined
+    return <div>Dang!</div>;
+}
 
 createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
