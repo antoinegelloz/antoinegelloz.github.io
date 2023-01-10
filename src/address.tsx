6673d77d73eadbca59a9f493939afa2756e5dcb7
Antoine Gelloz <antoine.gelloz@me.com>
2023-01-11 00:41:35 +0100

    deploy

--- src/address.tsx
+++ src/address.tsx
@@ -8,7 +8,7 @@ import {
     Heading,
     Input,
     InputGroup,
-    InputRightElement
+    InputRightElement, Spinner
 } from "@chakra-ui/react";
 import {GeoJSON} from "./models";
 
@@ -55,6 +55,7 @@ function Address(props: { adID: number }) {
     const [preciseAddress, setPreciseAddress] = useState<GeoJSON>(emptyGeoJSON)
     const [message, setMessage] = useState<string>("")
     const [error, setError] = useState<string>("")
+    const [loading, setLoading] = useState<boolean>(false)
 
     const searchAddress = async (address: string) => {
         if (address.length < 5) {
@@ -83,6 +84,7 @@ function Address(props: { adID: number }) {
         if (preciseAddress == emptyGeoJSON) {
             return
         }
+        setLoading(true)
         setPreciseAddress(emptyGeoJSON)
         setError("")
         let resp: HttpResponse<string>;
@@ -90,17 +92,18 @@ function Address(props: { adID: number }) {
             resp = await putJSON<string>(
                 'https://immo.gelloz.org/api/ads/' + props.adID.toString() + '/geojson',
                 {geojson: preciseAddress})
-            if (resp.parsedBody != undefined) {
-                setMessage(resp.parsedBody)
-            } else if (resp.status === 200) {
-                setMessage("Nouvelle adresse : " + preciseAddress.features[0].properties.label)
-            } else {
-                setMessage(resp.status.toString() + " " + resp.statusText)
+            if (resp.status != 200) {
+                console.log("validateAddress error", resp);
+                setError(JSON.stringify(resp, null, "\t"))
+                return
             }
+            console.log("validateAddress OK", preciseAddress);
+            setMessage("Nouvelle adresse : " + preciseAddress.features[0].properties.label)
         } catch (err) {
-            console.log("validateAddress error", err);
+            console.log("validateAddress error caught", err);
             setError(JSON.stringify(err, null, "\t"))
-            setPreciseAddress(emptyGeoJSON)
+        } finally {
+            setLoading(false)
         }
     }
 
@@ -138,7 +141,7 @@ function Address(props: { adID: number }) {
                 >
                     <AlertIcon boxSize='40px' mr={0}/>
                     <AlertTitle mt={4} mb={1} fontSize='lg'>
-                        Erreur !
+                        Erreur
                     </AlertTitle>
                     <AlertDescription maxWidth='sm'>
                         {error}
@@ -157,10 +160,29 @@ function Address(props: { adID: number }) {
                 >
                     <AlertIcon boxSize='40px' mr={0}/>
                     <AlertTitle mt={4} mb={1} fontSize='lg'>
-                        En cours
+                        Adresse modifiée
                     </AlertTitle>
                     <AlertDescription maxWidth='sm'>
                         {message}
+                    </AlertDescription>
+                </Alert> : <></>
+            }
+            {loading ?
+                <Alert
+                    status='info'
+                    variant='subtle'
+                    flexDirection='column'
+                    alignItems='center'
+                    justifyContent='center'
+                    textAlign='center'
+                    height='200px'
+                >
+                    <AlertIcon boxSize='40px' mr={0}/>
+                    <AlertTitle mt={4} mb={1} fontSize='lg'>
+                        En cours
+                    </AlertTitle>
+                    <AlertDescription maxWidth='sm'>
+                        <Spinner></Spinner>
                     </AlertDescription>
                 </Alert> : <></>
             }
